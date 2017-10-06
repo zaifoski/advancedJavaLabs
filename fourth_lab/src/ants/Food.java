b334d796ee11b38c9fa030f17358b4b7e1e3e6ec
zaifoski <sofiachimirri@gmail.com>
2017-10-06 14:59:47 +0200

    exercise 1, food class

---
+++ fourth_lab/src/ants/Food.java
@@ -0,0 +1,26 @@
+package ants;
+
+public class Food {
+	
+	private int amount;
+	
+	public Food(int amount){
+		this.amount = amount;
+	}
+
+	public synchronized int getAmount() {
+		return amount;
+	}
+
+	public synchronized Boolean removeFood(){
+		if(this.amount > 0) {
+			amount -= 1;
+			return true;
+		}
+		return false;
+	}
+
+	public synchronized void addFood(int value){
+		amount += value;
+	}
+}
